27dbefb7bfb6bcea10ce148cf7d0b832bd1cc9e8
Nico <nicopenaloza@gmail.com>
2021-08-26 11:15:46 -0300

    finalizado

--- app.js
+++ app.js
@@ -3,27 +3,49 @@
 const express = require('express');
 const config = require('./config');
 const fetch = require('node-fetch');
+const cors = require('cors');
 const app = express();
+const dbController = require('./modulos/dbController');
+
+app.use(cors());
+
+
+const traerDatosPeriodicos = async () => {
+    setTimeout(async () => {
+        
+        let respuesta = [];
+
+        for (const e of config.equipos){
+            await fetch('http://' + e.ip + ':' + e.puerto + '/info')
+            .then(r => r.text())
+            .then(body => JSON.parse(body))
+            .then(data => respuesta.push({id: e.id, nombre: e.nombre, data}))
+            .catch(err => console.error(err));
+        }
+
+        dbController.db.guardarNuevaEntrada(respuesta);
+
+        traerDatosPeriodicos();
+    }, 300000); // 300000ms = 5min
+}
+
 
 app.listen(config.puerto, () => { // Especifico que el servidor API va a iniciar en el puerto config.puerto
     console.log('API iniciada en el puerto:', config.puerto);
+    traerDatosPeriodicos();
 });
 
-app.get('/iar/api/:id', (req, res) => {
-    res.send('Te estoy trayendo los datos de "' + req.params.id + '"');
+app.get('/iar/api/getone/:id', (req, res) => {
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    dbController.db.traerLogsDeUnEquipo(req.params.id, res);
 });
 
-app.get('/iar/api/test/allData', async (req, res) => {
+app.get('/iar/api/getall', async (req, res) => {
     
-    let respuesta = [];
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
 
-    for (const e of config.equipos){
-        await fetch('http://' + e.ip + ':' + e.puerto + '/info')
-        .then(r => r.text())
-        .then(body => JSON.parse(body))
-        .then(data => respuesta.push({equipo: e.nombre, data}))
-        .catch(err => console.error(err));
-    }
-
-    res.send(respuesta)
+    dbController.db.traerLogs(res);
 });
--- modulos/dbController.js
+++ modulos/dbController.js
@@ -1,20 +1,77 @@
 const mysql = require('mysql');
 
-class db {
-    constructor (host, user, password, db) {
+class dbC {
+    constructor () {
         this.db = mysql.createConnection({
-            host: host,
-            user: user,
-            password: password,
-            database: db_name,
+            host: '127.0.0.1',
+            user: 'root',
+            password: '',
+            database: 'iar',
         });
     }
 
-    test () {
-        return 'eeeeeeeeeeaaaaa';
+    guardarNuevaEntrada(arr) {
+        arr.map(equipo => {
+            console.log(equipo.data)
+            this.db.query(
+                "INSERT INTO logs (`id_pc`, `nombre`, `data`, `fecha`) VALUES (?, ?, ?, CURRENT_TIMESTAMP())",
+                [equipo.id, equipo.nombre, JSON.stringify(equipo.data)],
+                (err, res) => {if (err) throw err; console.log(equipo.nombre, 'Guardado en la db')}
+            );
+            
+        });
+    }
+
+    traerLogs(ress) {
+        this.db.query(
+            "SELECT * FROM logs", 
+            (err, res) => {
+                if (err) throw err;
+
+                let result = [];
+
+                res.map(r => {
+                    result.push({
+                        id: r.id,
+                        id_pc: r.id_pc,
+                        nombre: r.nombre,
+                        data: JSON.parse(r.data),
+                        fecha: r.fecha
+                    })
+                })
+
+                ress.json(result);
+            }
+        )
+    }
+
+    traerLogsDeUnEquipo(equipo, ress) {
+        this.db.query(
+            "SELECT * FROM logs WHERE `id_pc` = ?",
+            equipo, 
+            (err, res) => {
+                if (err) throw err;
+
+                let result = [];
+
+                res.map(r => {
+                    result.push({
+                        id: r.id,
+                        id_pc: r.id_pc,
+                        nombre: r.nombre,
+                        data: JSON.parse(r.data),
+                        fecha: r.fecha
+                    })
+                })
+
+                ress.json(result);
+            }
+        )
     }
 
 
 }
 
+
+const db = new dbC();
 exports.db = db;
